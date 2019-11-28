7dc1d3718f434f82f56b7aee374a1a631619f547
Bikram Tuladhar <bikramtuladhar2011@gmail.com>
2019-11-28 16:28:45 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# simple-php-application
+
+## command to run
+``` php -S 0.0.0.0:3000 ```
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simple PHP application</title>
+</head>
+<body>
+<h1>
+<?php
+echo "Today is " . date("Y/m/d h:i:s");
+?>
+</h1>
+</body>
+</html>
